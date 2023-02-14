bfeba0a5ca26ab846a27ef02c2c29e8dcf4d3ba1
KimYongKwon <dydrnjs518@gmail.com>
2023-02-14 18:24:46 +0900

    fix: 모든 필드를 매개변수로 갖는 생성자가 없어서@builder에서 에러가 발생하는 버그 수정

--- src/main/java/com/monalisa/domain/member/dto/AddMemberRequestDto.java
+++ src/main/java/com/monalisa/domain/member/dto/AddMemberRequestDto.java
@@ -1,9 +1,6 @@
 package com.monalisa.domain.member.dto;
 
-import lombok.AccessLevel;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 import javax.validation.constraints.NotBlank;
 
@@ -11,6 +8,7 @@ import javax.validation.constraints.NotBlank;
 @Getter
 @Builder
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
 public class AddMemberRequestDto {
 
     @NotBlank
