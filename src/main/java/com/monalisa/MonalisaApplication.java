491d7b7ea4fc896e815a831d5320dac0cf362fa2
KimYongKwon <dydrnjs518@gmail.com>
2023-02-18 17:44:24 +0900

    refactor: Order에 생성시간 추가, Order에 종합주문가격 필드 추가

--- src/main/java/com/monalisa/MonalisaApplication.java
+++ src/main/java/com/monalisa/MonalisaApplication.java
@@ -2,7 +2,9 @@ package com.monalisa;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class MonalisaApplication {
     public static void main(String[] args) {
--- src/main/java/com/monalisa/domain/order/domain/Order.java
+++ src/main/java/com/monalisa/domain/order/domain/Order.java
@@ -1,6 +1,7 @@
 package com.monalisa.domain.order.domain;
 
 import com.monalisa.domain.book.domain.Book;
+import com.monalisa.global.domain.BaseTimeEntity;
 import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
 
@@ -9,19 +10,22 @@ import java.util.ArrayList;
 import java.util.List;
 
 @Entity
-@Table(name = "orderr")
+@Table(name = "orders")
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-public class Order {
+public class Order extends BaseTimeEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "order_id")
     private Long id;
 
+    private Integer totalPrice;
+
     @OneToMany(mappedBy = "order")
     private List<Book> bookList = new ArrayList<>();
 
     public Order(Book book) {
+        this.totalPrice = book.getCost();
         bookList.add(book);
         book.setOrder(this);
     }
