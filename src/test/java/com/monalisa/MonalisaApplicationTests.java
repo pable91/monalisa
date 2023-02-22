c151546a42c137b43b3688eab12a7012d5960ab0
KimYongKwon <dydrnjs518@gmail.com>
2023-02-22 16:48:08 +0900

    test:

--- src/test/java/com/monalisa/MonalisaApplicationTests.java
+++ src/test/java/com/monalisa/MonalisaApplicationTests.java
@@ -3,11 +3,11 @@ package com.monalisa;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
-class MonalisaApplicationTests {
-
-    @Test
-    void contextLoads() {
-    }
-
-}
+//@SpringBootTest
+//class MonalisaApplicationTests {
+//
+//    @Test
+//    void contextLoads() {
+//    }
+//
+//}
