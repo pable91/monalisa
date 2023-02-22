41ba0b63e2d1d83a2437551b56dd0253bdac9f51
KimYongKwon <dydrnjs518@gmail.com>
2023-02-22 19:26:33 +0900

    build: DB테이블과 data.sql의 테이블명이 대소문자가 서로 달라서 테이블을 못찾는 에러 수정

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -3,7 +3,7 @@ insert into USER (ACCOUNT_ID, PW, NAME) values
     ('AccountID2','$2a$10$eIIdfGFT8qJiavcREbl','lee'),
     ('AccountID3','$2a$10$eIIdfGFT8qJiavcREbl','park');
 
-insert into BOOK (name,description,cost,author,is_Sold,member_id,order_id) values
+insert into BOOK (NAME,DESCRIPTION,COST,AUTHOR,IS_SOLD,MEMBER_ID,ORDER_ID) values
 ('java', 'java book desciption', 5000, 'tom', false, 2, null),
 ('jpa', 'jpa book desciption', 7000, 'kevin', false, 2, null),
 ('spring', 'spring book desciption', 15000, 'k', false, 3, null);
