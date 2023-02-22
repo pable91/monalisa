43647741e422049ff57e3c3cc9ebcda81c2c7106
KimYongKwon <dydrnjs518@gmail.com>
2023-02-22 18:03:00 +0900

    build: 실제서버(aws)에서 사용될 yml과 build.gradle 설정 추가

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -3,7 +3,7 @@ insert into user (ACCOUNT_ID, PW, NAME) values
     ('AccountID2','$2a$10$eIIdfGFT8qJiavcREbl','lee'),
     ('AccountID3','$2a$10$eIIdfGFT8qJiavcREbl','park');
 
-insert into book (name,desc,cost,author,is_Sold,member_id,order_id) values
+insert into book (name,description,cost,author,is_Sold,member_id,order_id) values
 ('java', 'java book desciption', 5000, 'tom', false, 2, null),
 ('jpa', 'jpa book desciption', 7000, 'kevin', false, 2, null),
 ('spring', 'spring book desciption', 15000, 'k', false, 3, null);
