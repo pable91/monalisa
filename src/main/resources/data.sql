e939958380419b827cb9a3ba218c995557812c23
KimYongKwon <dydrnjs518@gmail.com>
2023-02-23 00:03:54 +0900

    fix: hibernate의 Physical Naming Strategy로 변경으로 인한 data.sql도 변경

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,9 +1,9 @@
-insert into USER (ACCOUNT_ID, PW, NAME) values
+insert into USER (account_id, pw, name) values
     ('AccountID1','$2a$10$eIIdfGFT8qJiavcREbl','kim'),
     ('AccountID2','$2a$10$eIIdfGFT8qJiavcREbl','lee'),
     ('AccountID3','$2a$10$eIIdfGFT8qJiavcREbl','park');
 
-insert into BOOK (NAME,DESCRIPTION,COST,AUTHOR,IS_SOLD,USER_ID,ORDER_ID) values
+insert into BOOK (name,description,cost,author,is_sold,user_id,order_id) values
     ('java', 'java book desciption', 5000, 'tom', false, 2, null),
     ('jpa', 'jpa book desciption', 7000, 'kevin', false, 2, null),
     ('spring', 'spring book desciption', 15000, 'k', false, 3, null);
