62acb919d6037717ba948a0ceae791b9547f25ec
KimYongKwon <dydrnjs518@gmail.com>
2023-02-24 00:01:20 +0900

    build: pid command 설정

--- src/main/java/com/monalisa/MonalisaApplication.java
+++ src/main/java/com/monalisa/MonalisaApplication.java
@@ -2,12 +2,16 @@ package com.monalisa;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.ApplicationPidFileWriter;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @EnableJpaAuditing
 @SpringBootApplication
 public class MonalisaApplication {
     public static void main(String[] args) {
-        SpringApplication.run(MonalisaApplication.class, args);
+        SpringApplication application = new SpringApplication(MonalisaApplication.class);
+        application.addListeners(new ApplicationPidFileWriter());
+        application.run(args);
     }
+
 }
