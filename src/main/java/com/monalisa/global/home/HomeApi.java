6ae064ea3fe915ac2bb80bd6e693c7a8c281a258
KimYongKwon <dydrnjs518@gmail.com>
2023-02-24 00:33:33 +0900

    CI / CD test

---
+++ src/main/java/com/monalisa/global/home/HomeApi.java
@@ -0,0 +1,17 @@
+package com.monalisa.global.home;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeApi {
+
+    @GetMapping("/")
+    public ResponseEntity<String> home() {
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body("ok");
+    }
+}
