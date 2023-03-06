c873dc2b25f66013e7af6b09dd8e6b8543b04baf
KimYongKwon <dydrnjs518@gmail.com>
2023-03-06 15:54:18 +0900

    test: data.sql 속성 추가

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -3,7 +3,7 @@ insert into USER (account_id, pw, name, email, create_date, user_role) values
     ('AccountID2','$2a$10$eIIdfGFT8qJiavcREbl','lee','lee@gmail.com', NOW(), 'NORMAL'),
     ('AccountID3','$2a$10$eIIdfGFT8qJiavcREbl','park','part@gmail.com', NOW(), 'NORMAL');
 
-insert into BOOK (name,description,cost,author,is_sold,user_id,order_id) values
-    ('java', 'java book desciption', 5000, 'tom', false, 2, null),
-    ('jpa', 'jpa book desciption', 7000, 'kevin', false, 2, null),
-    ('spring', 'spring book desciption', 15000, 'k', false, 3, null);
+insert into BOOK (name,description,cost,author,is_sold,user_id,order_id,likes) values
+    ('java', 'java book desciption', 5000, 'tom', false, 2, null,0),
+    ('jpa', 'jpa book desciption', 7000, 'kevin', false, 2, null,0),
+    ('spring', 'spring book desciption', 15000, 'k', false, 3, null,0);
