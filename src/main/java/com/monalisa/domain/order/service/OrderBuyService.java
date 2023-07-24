455acdb7a42e7f24551eb64fb86f64536ef4d651
김용권 <yongkwon.kim@ikoob.com>
2023-07-24 17:52:19 +0900

    IMP: 없는 책을 주문하려고 하면 예외를 던진다

--- src/main/java/com/monalisa/domain/order/service/OrderBuyService.java
+++ src/main/java/com/monalisa/domain/order/service/OrderBuyService.java
@@ -3,19 +3,19 @@ package com.monalisa.domain.order.service;
 import com.monalisa.domain.book.domain.Book;
 import com.monalisa.domain.book.exception.BookAlreadySoldException;
 import com.monalisa.domain.book.exception.BuyNotMyBookException;
+import com.monalisa.domain.book.exception.NotFoundBookException;
 import com.monalisa.domain.book.exception.error.BookErrorCode;
 import com.monalisa.domain.book.service.queryService.BookFindQueryService;
 import com.monalisa.domain.order.domain.Order;
 import com.monalisa.domain.order.dto.request.OrderRequestDto;
+import com.monalisa.domain.order.dto.request.OrderRequestDto.MultiBook;
 import com.monalisa.domain.order.dto.response.OrderResponseDto;
 import com.monalisa.domain.user.domain.User;
 import com.monalisa.domain.user.service.queryService.UserFindQueryService;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-
-import java.util.List;
-import java.util.stream.Collectors;
 
 @Service
 @Transactional
@@ -26,9 +26,10 @@ public class OrderBuyService {
     private final UserFindQueryService userFindQueryService;
     private final BookFindQueryService bookFindQueryService;
 
-    public OrderResponseDto.CreateSingle createOrderBySingleBook(final OrderRequestDto.SingleBook requestDto, final User user) {
-        final User buyer = userFindQueryService.findById(user.getId());
-        final Book targetBook = bookFindQueryService.findById(requestDto.getBookId());
+    public OrderResponseDto.CreateSingle createOrderBySingleBook(
+        final OrderRequestDto.SingleBook requestDto, final User user) {
+        User buyer = userFindQueryService.findById(user.getId());
+        Book targetBook = bookFindQueryService.findById(requestDto.getBookId());
 
         validate(buyer, requestDto, targetBook);
 
@@ -38,28 +39,38 @@ public class OrderBuyService {
         return OrderResponseDto.CreateSingle.of(targetBook, buyer.getName());
     }
 
-    private void validate(final User buyer, final OrderRequestDto.SingleBook requestDto, final Book book) {
+    private void validate(final User buyer, final OrderRequestDto.SingleBook requestDto,
+        final Book book) {
         if (book.isMine(buyer.getId())) {
             throw new BuyNotMyBookException(BookErrorCode.BUY_NOT_MY_BOOK, requestDto.getBookId());
         }
 
-        if(book.isSold()) {
-            throw new BookAlreadySoldException(BookErrorCode.BOOK_ALREADY_SOLD, requestDto.getBookId());
+        if (book.isSold()) {
+            throw new BookAlreadySoldException(BookErrorCode.BOOK_ALREADY_SOLD,
+                requestDto.getBookId());
         }
     }
 
-    public OrderResponseDto.CreateMulti createOrderByMultiBook(final OrderRequestDto.MultiBook requestDto, final User buyer) {
-        final List<Book> bookList = bookFindQueryService.findAllByIds(requestDto.getBookIds());
+    public OrderResponseDto.CreateMulti createOrderByMultiBook(
+        final OrderRequestDto.MultiBook requestDto, final User buyer) {
+        List<Book> bookList = bookFindQueryService.findAllByIds(requestDto.getBookIds());
 
-        // TODO 만약 없는 책을 구입하려 한다면?
+        validate(requestDto, bookList);
 
-        // 판매 책은 제외
-        final List<Book> bookListByNotSold = bookList.stream()
-                .filter(b -> !b.isSold())
-                .collect(Collectors.toUnmodifiableList());
+        List<Book> bookListByNotSold = bookList.stream()
+            .filter(b -> !b.isSold())
+            .toList();
 
         Order newOrder = Order.createOrderByMultiBook(bookListByNotSold, buyer);
 
-        return OrderResponseDto.CreateMulti.of(orderUpdateQueryService.save(newOrder).getBookList(), buyer);
+        return OrderResponseDto.CreateMulti.of(orderUpdateQueryService.save(newOrder).getBookList(),
+            buyer);
+    }
+
+    private void validate(MultiBook requestDto, List<Book> bookList) {
+        // 굳이 필요할까?
+        if (bookList.size() != requestDto.getBookIds().size()) {
+            throw new NotFoundBookException(BookErrorCode.BOOK_NOT_FOUND);
+        }
     }
 }
